e85e1123eae697b9644b31fa2bbb21d95f39c36b
Viachaslau Markevich <inbox@enod.org>
2012-08-30 22:36:47 +0300

    create tower class

---
+++ Conveyor Defence/Conveyor Defence/Nodes/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conveyor_Defence.Nodes
+{
+    class Tower:Node
+    {
+        public Tower(float outputCooldown = 0) : base(outputCooldown)
+        {
+
+        }
+
+        private void Shoot(int x, int y)
+        {
+            
+        }
+    }
+}
