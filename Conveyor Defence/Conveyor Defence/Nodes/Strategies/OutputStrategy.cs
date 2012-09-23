84b82814dbdbdc39e7245a9341de2900bc21ecdd
Viachaslau Markevich <inbox@enod.org>
2012-09-24 00:33:23 +0300

    add abstraction for output and process strategies

---
+++ Conveyor Defence/Conveyor Defence/Nodes/Strategies/OutputStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Conveyor_Defence.Missiles;
+
+namespace Conveyor_Defence.Nodes.Strategies
+{
+    abstract class OutputStrategy
+    {
+        public abstract void Output(Missile missile);
+    }
+}
---
+++ Conveyor Defence/Conveyor Defence/Nodes/Strategies/ProcessStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Conveyor_Defence.Missiles;
+
+namespace Conveyor_Defence.Nodes.Strategies
+{
+    abstract class ProcessStrategy
+    {
+        public abstract void Process(Missile missile);
+    }
+}
